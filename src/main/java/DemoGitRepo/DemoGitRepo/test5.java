87284d882d3d9e0d60b6e167b50a2c6c1553b725
dadasahebnkhade <dadasahebnkhade@gmail.com>
2022-12-21 18:42:19 +0530

    added syso for 2 diff

--- src/main/java/DemoGitRepo/DemoGitRepo/test5.java
+++ src/main/java/DemoGitRepo/DemoGitRepo/test5.java
@@ -5,6 +5,7 @@ public class test5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("this is test5 file for difference verification");
+     System.out.println("this is test5 file for difference validation");
 	}
 
 }
